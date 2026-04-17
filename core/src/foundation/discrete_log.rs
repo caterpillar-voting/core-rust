2c81286f29eaf75d05af980a0b78672adf7d4d17
Florian Moser <git@famoser.ch>
2026-04-17 15:37:23 +0200

    ref: Clarify name of file, improve naming

--- core/src/foundation/discrete_log.rs
+++ core/src/foundation/discrete_log.rs
@@ -4,17 +4,17 @@ pub trait DiscreteLog<G: Group> {
     fn log(&self, g: &G::Point, point: &G::Point) -> Option<G::Scalar>;
 }
 
-pub struct DiscreteLogRange<'a, G: Group> {
+pub struct GreedyDiscreteLog<'a, G: Group> {
     range: &'a (G::Scalar, G::Scalar)
 }
 
-impl<'a, G: Group> DiscreteLogRange<'a, G> {
+impl<'a, G: Group> GreedyDiscreteLog<'a, G> {
     pub(crate) fn new(m_range: &'a (G::Scalar, G::Scalar)) -> Self {
         Self { range: m_range }
     }
 }
 
-impl<'a, G: Group> DiscreteLog<G> for DiscreteLogRange<'a, G> {
+impl<'a, G: Group> DiscreteLog<G> for GreedyDiscreteLog<'a, G> {
     fn log(&self, g: &G::Point, point: &G::Point) -> Option<G::Scalar> {
         let mut current = self.range.0;
         loop {
