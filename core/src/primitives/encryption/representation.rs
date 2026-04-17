725db802459a61e022a103052bc8f33687f6d62f
Florian Moser <git@famoser.ch>
2026-04-17 15:20:12 +0200

    ref: Use wrapper types

--- core/src/primitives/encryption/representation.rs
+++ core/src/primitives/encryption/representation.rs
@@ -3,36 +3,26 @@ use zeroize::{Zeroize, ZeroizeOnDrop};
 use crate::foundation::group::Group;
 
 #[derive(Clone, Debug, PartialEq, Zeroize, ZeroizeOnDrop)]
-pub struct SecretKey<G: Group> {
-    pub inner: G::Scalar,
-}
+pub struct SecretKey<G: Group>(pub G::Scalar);
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct PublicKey<G: Group> {
-    pub inner: G::Point,
-}
+pub struct PublicKey<G: Group>(pub G::Point);
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct Ciphertext<G: Group> {
-    pub alpha: G::Point,
-    pub beta: G::Point,
-    // TODO: include ZKP for CCA2
-}
+pub struct Ciphertext<G: Group>(pub G::Point, pub G::Point);
+// TODO: include ZKP for CCA2
 
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct HomomorphicCiphertext<G: Group> {
-    pub alpha: G::Point,
-    pub beta: G::Point,
-    // TODO: include ZKP for CCA2
-}
+pub struct HomomorphicCiphertext<G: Group>(pub G::Point, pub G::Point);
+// TODO: include ZKP for CCA2
 
 impl<G: Group> ops::Add<&HomomorphicCiphertext<G>> for &HomomorphicCiphertext<G> {
     type Output = HomomorphicCiphertext<G>;
     fn add(self, rhs: &HomomorphicCiphertext<G>) -> Self::Output {
         HomomorphicCiphertext {
-            alpha: self.alpha + &rhs.alpha,
-            beta: self.beta + &rhs.beta,
+            0: self.0 + &rhs.0,
+            1: self.1 + &rhs.1,
         }
     }
 }
@@ -41,8 +31,8 @@ impl<G: Group> ops::Mul<&G::Scalar> for &HomomorphicCiphertext<G> {
     type Output = HomomorphicCiphertext<G>;
     fn mul(self, rhs: &G::Scalar) -> Self::Output {
         HomomorphicCiphertext {
-            alpha: self.alpha * &rhs,
-            beta: self.beta * &rhs,
+            0: self.0 * &rhs,
+            1: self.1 * &rhs,
         }
     }
 }
@@ -52,8 +42,8 @@ impl<G: Group> ops::Sub<&HomomorphicCiphertext<G>> for &HomomorphicCiphertext<G>
 
     fn sub(self, rhs: &HomomorphicCiphertext<G>) -> Self::Output {
         HomomorphicCiphertext {
-            alpha: self.alpha - &rhs.alpha,
-            beta: self.beta - &rhs.beta,
+            0: self.0 - &rhs.0,
+            1: self.1 - &rhs.1,
         }
     }
 }
