787ca86415cbf33f93daf474747c07fa23de87e7
Florian Moser <git@famoser.ch>
2026-04-19 08:25:06 +0200

    test: Add test for tree structure

--- core/src/primitives/zkp/zkp.rs
+++ core/src/primitives/zkp/zkp.rs
@@ -11,20 +11,22 @@ pub struct Statement<G: Group> {
     z: G::Point,
 }
 
+type Transcript<G: Group> = (G::Scalar, G::Point);
+type Commit<G: Group> = (G::Scalar, G::Point);
 /// https://crypto.ethz.ch/publications/files/Maurer09.pdf
 impl<G: Group> Statement<G> {
     pub fn new(g: G::Point, z: G::Point) -> Self {
         Self { g, z }
     }
 
-    pub fn commit<R: RngCore + CryptoRng>(&self, rng: &mut R) -> (G::Scalar, G::Point) {
+    pub fn commit<R: RngCore + CryptoRng>(&self, rng: &mut R) -> Commit<G> {
         let k = G::scalar_random(rng);
         let t = self.g * &k;
 
         (k, t)
     }
 
-    pub fn proof(&self, k: &G::Scalar, c: &G::Scalar, x: &G::Scalar) -> G::Scalar {
+    pub fn proof(&self, k: &G::Scalar, x: &G::Scalar, c: &G::Scalar) -> G::Scalar {
         let r = *k + &(*c * x);
 
         r
@@ -37,11 +39,7 @@ impl<G: Group> Statement<G> {
         left == right
     }
 
-    pub fn simulate<R: RngCore + CryptoRng>(
-        &self,
-        rng: &mut R,
-        c: &G::Scalar,
-    ) -> (G::Scalar, G::Point) {
+    pub fn simulate<R: RngCore + CryptoRng>(&self, rng: &mut R, c: &G::Scalar) -> Transcript<G> {
         let r = G::scalar_random(rng);
 
         let t = self.g * &r - &(self.z * &c);
@@ -56,29 +54,23 @@ pub enum BooleanTree<T> {
     Or(Vec<BooleanTree<T>>),
 }
 
-pub struct ZeroKnowledgeProof<G: Group> {
-    proof: BooleanTree<Box<[Statement<G>]>>,
-}
+pub struct ZeroKnowledgeProof {}
 
+type Claim<G: Group> = BooleanTree<Box<[Statement<G>]>>;
 type Knowledge<G: Group> = BooleanTree<Option<G::Scalar>>;
-type CommittedProof<G: Group> = BooleanTree<Box<[(G::Scalar, G::Point)]>>;
-type SimulatedProof<G: Group> = BooleanTree<(G::Scalar, Box<[(G::Scalar, G::Point)]>)>;
+type CommittedProof<G: Group> = BooleanTree<Box<[Commit<G>]>>;
+type SimulatedProof<G: Group> = BooleanTree<(G::Scalar, Box<[Transcript<G>]>)>;
 type PreparedProof<G: Group> = BooleanTree<(Option<CommittedProof<G>>, Option<SimulatedProof<G>>)>;
-
-pub struct Proof<G: Group>(pub BooleanTree<Option<G::Scalar>>);
+type Proof<G: Group> = BooleanTree<(G::Scalar, Box<[Transcript<G>]>)>;
 
 /// https://crypto.ethz.ch/publications/files/Maurer09.pdf
-impl<G: Group> ZeroKnowledgeProof<G> {
-    pub fn new(proof: BooleanTree<Box<[Statement<G>]>>) -> Self {
-        Self { proof }
-    }
-
-    pub fn prepare<R: RngCore + CryptoRng>(
-        proof: &BooleanTree<Box<[Statement<G>]>>,
+impl ZeroKnowledgeProof {
+    pub fn prepare<G: Group, R: RngCore + CryptoRng>(
+        claim: &Claim<G>,
         knowledge: &Knowledge<G>,
         rng: &mut R,
     ) -> PreparedProof<G> {
-        match proof {
+        match claim {
             Leaf(statements) => {
                 assert!(matches!(knowledge, Leaf(Some(_))));
 
@@ -109,15 +101,13 @@ impl<G: Group> ZeroKnowledgeProof<G> {
                             if let Leaf(None) = &knowledge_node {
                                 let c = G::scalar_random(rng);
 
-                                return Leaf((None, Some(Self::simulate(node, rng, c))))
+                                return Leaf((None, Some(Self::simulate(node, rng, c))));
                             }
 
-                            // otherwise, I MUST commit, correct?
-                            // yes, but inside there might still be a simulated proof...
                             if let Leaf(statements) = &node {
                                 assert!(matches!(knowledge, Leaf(_)));
 
-                                return Leaf((Some(Self::commit(statements, rng)), None))
+                                return Leaf((Some(Self::commit(statements, rng)), None));
                             }
 
                             return Self::prepare(node, &knowledge_node, rng);
@@ -132,7 +122,7 @@ impl<G: Group> ZeroKnowledgeProof<G> {
         }
     }
 
-    fn commit<R: RngCore + CryptoRng>(
+    fn commit<G: Group, R: RngCore + CryptoRng>(
         statements: &Box<[Statement<G>]>,
         rng: &mut R,
     ) -> CommittedProof<G> {
@@ -144,7 +134,7 @@ impl<G: Group> ZeroKnowledgeProof<G> {
         Leaf(committed)
     }
 
-    fn simulate<R: RngCore + CryptoRng>(
+    fn simulate<G: Group, R: RngCore + CryptoRng>(
         proof: &BooleanTree<Box<[Statement<G>]>>,
         rng: &mut R,
         c: G::Scalar,
@@ -178,7 +168,39 @@ impl<G: Group> ZeroKnowledgeProof<G> {
         }
     }
 
-    pub fn verify(&self, proof: &Proof<G>, hash: &dyn ContextAwareHash<G>) -> bool {
+    pub fn proof<G: Group>(
+        claim: &Claim<G>,
+        prepared_proof: &PreparedProof<G>,
+        knowledge: &Knowledge<G>,
+        c: &G::Scalar,
+    ) -> Proof<G> {
+        Leaf((
+            G::Scalar::from(1u64),
+            vec![(G::Scalar::from(1), G::basepoint())]
+                .into_iter()
+                .collect(),
+        ))
+        /*
+        match claim {
+            Leaf(statements) => {
+                if let Leaf(committed_or_simulated) = prepared_proof {
+                    assert_ne!(committed_or_simulated.0.is_some(), committed_or_simulated.1.is_some());
+
+                    if committed_or_simulated.1.is_some() {
+                        assert_eq!(c, committed_or_simulated.1.unwrap());
+                        return committed_or_simulated.1
+                    }
+                }
+
+                unreachable!("proof and knowledge trees not synchronized")
+            }
+            And(nodes) => {}
+            Or(nodes) => {}
+        }
+        */
+    }
+
+    pub fn verify<G: Group>(&self, claim: &Claim<G>, proof: &Proof<G>) -> bool {
         false
     }
 }
@@ -193,7 +215,6 @@ mod tests {
     use crate::foundation::hash::VectorContextHash;
     use crate::primitives::encryption::el_gamal::{ElGamal, ExponentialElGamal};
     use rand::thread_rng;
-    use sha2::Sha512;
 
     type Curve = RistrettoGroup;
 
@@ -213,7 +234,7 @@ mod tests {
         context.add_context(&t);
         let c = context.hash();
 
-        let r = zkp.proof(&k, &c, &x);
+        let r = zkp.proof(&k, &x, &c);
         assert!(zkp.verify(&r, &t, &c));
     }
 
@@ -226,6 +247,46 @@ mod tests {
         let c = Curve::scalar_random(&mut rng);
         let (r, t) = zkp.simulate(&mut rng, &c);
         assert!(zkp.verify(&r, &t, &c));
+    }
+
+    #[test]
+    fn prepare_complex_statement() {
+        let mut rng = thread_rng();
+
+        let el_gamal = ElGamal::<Curve>::default();
+        let exponential_el_gamal = ExponentialElGamal(el_gamal);
+        let sk = exponential_el_gamal.0.generate_secret_key(&mut rng);
+        let pk = exponential_el_gamal.0.derive_public_key(&sk);
+
+        // encrypt 0
+        let r = Scalar::random(&mut rng);
+        let (u, v) = exponential_el_gamal.encrypt(&pk, &r, &Scalar::ZERO);
+
+        // re-encrypt
+        let r2 = Scalar::random(&mut rng);
+        let (u_dash, v_dash) = exponential_el_gamal.0.reencrypt(&pk, &r2, &(u, v));
+
+        // enc proof (simulated) and rerand proof (true)
+        let zkp_enc1_u = Statement::<Curve>::new(Curve::basepoint(), u_dash);
+        let zkp_enc1_v = Statement::<Curve>::new(pk, v_dash - Curve::basepoint());
+        let zkp_rerand_u = Statement::<Curve>::new(Curve::basepoint(), u_dash - u);
+        let zkp_rerand_v = Statement::<Curve>::new(pk, v_dash - v);
+
+        let claim: Claim<Curve> = Or(vec![
+            Leaf(Box::new([zkp_enc1_u.clone(), zkp_enc1_v.clone()])),
+            Leaf(Box::new([zkp_rerand_u.clone(), zkp_rerand_v.clone()])),
+        ]);
+        let knowledge: Knowledge<Curve> = Or(vec![Leaf(None), Leaf(Some(r2))]);
+        let prepared_proof = ZeroKnowledgeProof::prepare(&claim, &knowledge, &mut rng);
+
+        assert!(matches!(prepared_proof, Or(_)));
+        if let Or(nodes) = prepared_proof {
+            assert_eq!(nodes.len(), 2);
+            assert!(matches!(nodes[0], Leaf(_)));
+            assert!(matches!(nodes[1], Leaf(_)));
+        } else {
+            unreachable!()
+        }
     }
 
     #[test]
@@ -266,8 +327,8 @@ mod tests {
         let c = context.hash();
         let c2 = c - c1;
 
-        let r_rerand_u = zkp_rerand_u.proof(&k_rerand_u, &c2, &r2);
-        let r_rerand_v = zkp_rerand_v.proof(&k_rerand_v, &c2, &r2);
+        let r_rerand_u = zkp_rerand_u.proof(&k_rerand_u, &r2, &c2);
+        let r_rerand_v = zkp_rerand_v.proof(&k_rerand_v, &r2, &c2);
 
         assert!(zkp_enc1_u.verify(&r_enc1_u, &t_enc1_u, &c1));
         assert!(zkp_enc1_v.verify(&r_enc1_v, &t_enc1_v, &c1));
@@ -314,8 +375,8 @@ mod tests {
         let c = context.hash();
         let c1 = c - c2;
 
-        let r_enc1_u = zkp_enc1_u.proof(&k_enc1_u, &c1, &r2);
-        let r_enc1_v = zkp_enc1_v.proof(&k_enc1_v, &c1, &r2);
+        let r_enc1_u = zkp_enc1_u.proof(&k_enc1_u, &r2, &c1);
+        let r_enc1_v = zkp_enc1_v.proof(&k_enc1_v, &r2, &c1);
 
         assert!(zkp_enc1_u.verify(&r_enc1_u, &t_enc1_u, &c1));
         assert!(zkp_enc1_v.verify(&r_enc1_v, &t_enc1_v, &c1));
