23fe14cf74564b718595ca094e5df138aef0d881
Florian Moser <git@famoser.ch>
2026-04-20 16:34:18 +0200

    feat: Implement OR builder

--- core/src/primitives/zkp/proof_builder.rs
+++ core/src/primitives/zkp/proof_builder.rs
@@ -6,39 +6,37 @@ use crate::utils::tree::BooleanTree::{And, Or};
 use std::marker::PhantomData;
 
 pub trait ProofBuilder<G: Group> {
-    fn build(self) -> (Claim<G>, Knowledge<G>);
+    fn build(&self) -> (Claim<G>, Knowledge<G>);
 }
 
-pub struct AndProofBuilder<G: Group, P: ProofBuilder<G>>
-where
-    P: Sized,
+pub struct OrProofBuilder<'a, G: Group>
 {
-    proof_builders: Vec<P>,
+    proof_builders: Vec<&'a dyn ProofBuilder<G>>,
     _marker: PhantomData<G>,
 }
 
-impl<G: Group, P: ProofBuilder<G>> Default for AndProofBuilder<G, P> {
+impl<'a, G: Group> Default for OrProofBuilder<'a, G> {
     fn default() -> Self {
         Self::new(Vec::new())
     }
 }
 
-impl<G: Group, P: ProofBuilder<G>> AndProofBuilder<G, P> {
-    pub fn new(proof_builders: Vec<P>) -> Self {
+impl<'a, G: Group> OrProofBuilder<'a, G> {
+    pub fn new(proof_builders: Vec<&'a dyn ProofBuilder<G>>) -> Self {
         Self {
             proof_builders,
             _marker: PhantomData,
         }
     }
 
-    pub fn and(&mut self, proof_builder: P) -> &mut AndProofBuilder<G, P> {
+    pub fn or(&mut self, proof_builder: &'a dyn ProofBuilder<G>) -> &mut OrProofBuilder<'a, G> {
         self.proof_builders.push(proof_builder);
 
         self
     }
 
-    fn collect_proof_builders(proof_builders: Vec<P>) -> (Vec<Claim<G>>, Vec<Knowledge<G>>) {
-        proof_builders.into_iter().fold(
+    fn collect_proof_builders(&self) -> (Vec<Claim<G>>, Vec<Knowledge<G>>) {
+        self.proof_builders.iter().fold(
             (Vec::new(), Vec::new()),
             |(mut claims, mut knowledges), pb| {
                 let (claim, knowledge) = pb.build();
@@ -50,48 +48,75 @@ impl<G: Group, P: ProofBuilder<G>> AndProofBuilder<G, P> {
     }
 }
 
-impl<G: Group, P: ProofBuilder<G>> ProofBuilder<G> for AndProofBuilder<G, P> {
-    fn build(self) -> (Claim<G>, Knowledge<G>) {
-        let (claims, knowledges) = Self::collect_proof_builders(self.proof_builders);
-
-        (And(claims), And(knowledges))
-    }
-}
-
-
-pub struct OrProofBuilder<G: Group, P: ProofBuilder<G>>
-where
-    P: Sized,
-{
-    proof_builders: Vec<P>,
-    _marker: PhantomData<G>,
-}
-
-impl<G: Group, P: ProofBuilder<G>> Default for OrProofBuilder<G, P> {
-    fn default() -> Self {
-        Self::new(Vec::new())
-    }
-}
-
-impl<G: Group, P: ProofBuilder<G>> OrProofBuilder<G, P> {
-    pub fn new(proof_builders: Vec<P>) -> Self {
-        Self {
-            proof_builders,
-            _marker: PhantomData,
-        }
-    }
-
-    pub fn or(&mut self, proof_builder: P) -> &mut OrProofBuilder<G, P> {
-        self.proof_builders.push(proof_builder);
-
-        self
-    }
-}
-
-impl<G: Group, P: ProofBuilder<G>> ProofBuilder<G> for OrProofBuilder<G, P> {
-    fn build(self) -> (Claim<G>, Knowledge<G>) {
-        let (claims, knowledges) = AndProofBuilder::collect_proof_builders(self.proof_builders);
+impl<'a, G: Group> ProofBuilder<G> for OrProofBuilder<'a, G> {
+    fn build(&self) -> (Claim<G>, Knowledge<G>) {
+        let (claims, knowledges) = self.collect_proof_builders();
 
         (Or(claims), Or(knowledges))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::foundation::group::Group;
+    use crate::foundation::group::ristretto::RistrettoGroup;
+    use crate::primitives::encryption::el_gamal::{ElGamal, ExponentialElGamal};
+    use rand::thread_rng;
+    use rand_core::{CryptoRng, RngCore};
+    use crate::primitives::zkp::proof::ZeroKnowledgeProof;
+    use crate::primitives::zkp::proof_builder::el_gamal::{EncProof, ReEncProof};
+
+    type Curve = RistrettoGroup;
+
+    type Scalar = <RistrettoGroup as Group>::Scalar;
+    type Point = <RistrettoGroup as Group>::Point;
+
+    fn create<R: RngCore + CryptoRng>(
+        rng: &mut R,
+    ) -> (Point, (Point, Point), (Point, Point, Scalar)) {
+        let el_gamal = ElGamal::<Curve>::default();
+        let exponential_el_gamal = ExponentialElGamal(el_gamal);
+        let sk = exponential_el_gamal.0.generate_secret_key(rng);
+        let pk = exponential_el_gamal.0.derive_public_key(&sk);
+
+        // encrypt 0
+        let r = Scalar::random(rng);
+        let (u, v) = exponential_el_gamal.encrypt(&pk, &r, &Scalar::ZERO);
+
+        // encrypt 1
+        let r_enc1 = Scalar::random(rng);
+        let (u_enc1, v_enc1) = exponential_el_gamal.encrypt(&pk, &r_enc1, &Scalar::ONE);
+
+        (pk, (u, v), (u_enc1, v_enc1, r_enc1))
+    }
+
+    fn check_proof_valid<R: RngCore + CryptoRng>(
+        rng: &mut R,
+        claim: Claim<Curve>,
+        knowledge: Knowledge<Curve>,
+    ) {
+        let prepared_proof = ZeroKnowledgeProof::prepare(rng, &claim, &knowledge);
+        let challenge = Scalar::random(rng);
+        let finalized_proof =
+            ZeroKnowledgeProof::finalize(rng, &prepared_proof, &claim, &knowledge, &challenge);
+
+        assert!(ZeroKnowledgeProof::check(
+            &claim,
+            &finalized_proof,
+            &challenge))
+    }
+
+    #[test]
+    fn zkp_OR_proof() {
+        let mut rng = thread_rng();
+
+        let (pk, (u, v), (u_enc1, v_enc1, r_enc1)) = create(&mut rng);
+
+        let enc1 = EncProof::<Curve>::new(pk, (u_enc1, v_enc1), Curve::basepoint(), Some(r_enc1));
+        let renenc = ReEncProof::<Curve>::new(pk, (u, v), (u_enc1, v_enc1), None);
+        let (claim, knowledge) = OrProofBuilder::<Curve>::new(vec![&enc1, &renenc]).build();
+
+        check_proof_valid(&mut rng, claim, knowledge);
     }
 }
