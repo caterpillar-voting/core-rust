0f3efbe8e1eb6249f91b9028a711cd775dd004f9
Florian Moser <git@famoser.ch>
2026-04-21 10:17:30 +0200

    ref: Precise context hash

--- core/src/foundation/hash.rs
+++ core/src/foundation/hash.rs
@@ -1,42 +1,39 @@
 use crate::foundation::group::{ByteSerialize, Group};
 
-pub trait ContextAwareHash<G: Group> {
-    fn add_context(&mut self, p: &G::Point);
-    fn hash(&self) -> G::Scalar;
+pub trait ContextHash<G: Group> {
+    fn add_point(&mut self, p: &G::Point);
+    fn hash_to_scalar(&self) -> G::Scalar;
+    fn hash_to_point(&self) -> G::Point;
 }
 
-pub struct VectorContextHash<G: Group> {
+pub struct VectorContextHash {
     context: Vec<u8>,
-    _marker: core::marker::PhantomData<G>,
 }
 
-impl<G: Group> VectorContextHash<G> {
-    pub fn new(global: Vec<G::Point>) -> Self {
-        let mut own = Self {
-            context: Vec::new(),
-            _marker: core::marker::PhantomData,
-        };
-
-        global.iter().for_each(|p| own.add_context(p));
-
-        own
-    }
-}
-
-impl<G: Group> Default for VectorContextHash<G> {
+impl Default for VectorContextHash {
     fn default() -> Self {
         Self::new(Vec::new())
     }
 }
 
-impl<G: Group> ContextAwareHash<G> for VectorContextHash<G> {
-    fn add_context(&mut self, p: &G::Point) {
+impl VectorContextHash {
+    pub fn new(context: Vec<u8>) -> Self {
+        Self { context }
+    }
+}
+
+impl<G: Group> ContextHash<G> for VectorContextHash {
+    fn add_point(&mut self, p: &G::Point) {
         let mut bytes = vec![0u8; G::Point::BUFFER_SIZE];
         p.to_bytes(&mut bytes[..]);
         self.context.extend_from_slice(&bytes[..]);
     }
 
-    fn hash(&self) -> G::Scalar {
+    fn hash_to_scalar(&self) -> G::Scalar {
         G::hash_to_scalar(&self.context[..])
+    }
+
+    fn hash_to_point(&self) -> G::Point {
+        G::hash_to_point(&self.context[..])
     }
 }
