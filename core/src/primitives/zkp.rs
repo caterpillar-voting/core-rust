57707f873b21992e2e6f5f0ff184d51be585039d
Florian Moser <git@famoser.ch>
2026-04-21 11:16:22 +0200

    test: Implement ZKP test

--- core/src/primitives/zkp.rs
+++ core/src/primitives/zkp.rs
@@ -2,6 +2,8 @@ use crate::foundation::group::Group;
 use crate::foundation::hash::ContextHash;
 use crate::primitives::zkp::context::ProofTreeContextHash;
 use crate::primitives::zkp::proof::{Claim, Knowledge, PreparedProof, Proof, ProofTree};
+use crate::primitives::zkp::proof_builder::ProofTreeBuilder;
+use crate::utils::tree::BooleanTree::{And, Or};
 use rand_core::{CryptoRng, RngCore};
 
 pub mod proof;
@@ -69,20 +71,48 @@ impl<G: Group, H: ProofTreeContextHash<G> + ContextHash<G> + Clone>
         context_hash.add_prepared_proof(&prepared_proof);
         let c = context_hash.hash_to_scalar();
 
-        ProofTree::finalize(
-            rng,
-            &prepared_proof,
-            &self.zero_knowledge_proof.claim,
-            knowledge,
-            &c,
-        )
+        self.zero_knowledge_proof.finalize(rng, &prepared_proof, knowledge, &c)
     }
 
-    pub fn verify(&self, claim: &Claim<G>, proof: &Proof<G>) -> bool {
+    pub fn verify(&self, proof: &Proof<G>) -> bool {
         let mut context_hash = self.claim_context_hash.clone();
         context_hash.add_proof(proof);
         let c = context_hash.hash_to_scalar();
 
-        ProofTree::check(claim, proof, &c)
+        self.zero_knowledge_proof.check(proof, &c)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::foundation::group::Group;
+    use crate::foundation::group::ristretto::RistrettoGroup;
+    use crate::primitives::zkp::_test_utils::{create_enc0_and_enc1, do_proof};
+    use crate::primitives::zkp::proof_builder::ProofBuilder;
+    use crate::primitives::zkp::proof_builder::el_gamal::{EncProof, ReEncProof};
+    use crate::utils::tree::BooleanTree::Leaf;
+    use rand::thread_rng;
+    use crate::foundation::hash::VectorContextHash;
+
+    type Curve = RistrettoGroup;
+
+    #[test]
+    fn non_interactive_zero_knowledge_proof() {
+        let mut rng = thread_rng();
+
+        let (pk, (u, v), (u_enc1, v_enc1, r_enc1)) = create_enc0_and_enc1(&mut rng);
+
+        let enc1 = EncProof::<Curve>::new(pk, (u_enc1, v_enc1), Curve::basepoint(), Some(r_enc1));
+        let renenc = ReEncProof::<Curve>::new(pk, (u, v), (u_enc1, v_enc1), None);
+
+        let tree = ProofTreeBuilder::new(Or(vec![Leaf(&enc1), Leaf(&renenc)]));
+        let (claim, knowledge) = tree.build();
+
+        let zkp = ZeroKnowledgeProof { claim };
+        let nizkp = NonInteractiveZeroKnowledgeProof::new(zkp, VectorContextHash::default());
+        let proof = nizkp.proof(&mut rng, &knowledge);
+
+        assert!(nizkp.verify(&proof))
     }
 }
