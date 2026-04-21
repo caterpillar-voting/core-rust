e768eb7608f80f426f709d3e8482856c7c888f4c
Florian Moser <git@famoser.ch>
2026-04-21 13:40:09 +0200

    test: Add showcase of ZKP

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -10,6 +10,11 @@ mod tests {
     use crate::primitives::commitment::HHomomorphicCommitment;
     use crate::primitives::encryption::{Encryption, EncryptionHomomorph};
     use rand::thread_rng;
+    use crate::foundation::hash::VectorContextHash;
+    use crate::primitives::zkp::proof_builder::el_gamal::{EncProofBuilder, ReEncProofBuilder};
+    use crate::primitives::zkp::proof_builder::TreeProofBuilder;
+    use crate::primitives::zkp::{NIZKProof, ZKProof};
+    use crate::utils::tree::BooleanTree::{Leaf, Or};
 
     type Curve = RistrettoGroup;
     type Scalar = <RistrettoGroup as Group>::Scalar;
@@ -55,5 +60,28 @@ mod tests {
         let message_recovered = encryption.decrypt(&secret_key, &ciphertext_sum, &decoder);
 
         assert_eq!(message_recovered, Some(message_sum));
+    }
+
+    #[test]
+    fn nizk_proof() {
+        let mut rng = thread_rng();
+        let message1 = Curve::point_random(&mut rng);
+        let message2 = Curve::point_random(&mut rng);
+
+        let encryption = Encryption::<Curve>::default();
+        let (secret_key, public_key) = encryption.key_gen(&mut rng);
+
+        let randomness = Curve::scalar_random(&mut rng);
+        let ciphertext = encryption.el_gamal.encrypt(&public_key, &randomness, &message1);
+
+        let enc1 = EncProofBuilder::<Curve>::new(public_key, ciphertext, message1, Some(randomness));
+        let enc2 = EncProofBuilder::<Curve>::new(public_key, ciphertext, message2, None);
+        let tree: TreeProofBuilder<Curve> = Or(vec![Leaf(&enc1), Leaf(&enc2)]);
+        let (zk_proof, knowledge) = ZKProof::from_builder(&tree);
+
+        let nizkp = NIZKProof::new(zk_proof, VectorContextHash::default());
+        let proof = nizkp.proof(&mut rng, &knowledge);
+
+        assert!(nizkp.verify(&proof))
     }
 }
