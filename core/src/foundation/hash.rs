53f5510781094d0051f11215f1ac217f901e4a1b
Pierrick Gaudry <pierrick.gaudry@loria.fr>
2026-04-24 13:47:28 +0200

    Trying or-proof; doesn't work yet: doesn't recurse.

--- core/src/foundation/hash.rs
+++ core/src/foundation/hash.rs
@@ -2,6 +2,7 @@ use crate::foundation::group::{ByteSerialize, Group};
 
 pub trait ContextHash<G: Group> {
     fn add_point(&mut self, p: &G::Point);
+    fn add_scalar(&mut self, s: &G::Scalar);
     fn hash_to_scalar(&self) -> G::Scalar;
     fn hash_to_point(&self) -> G::Point;
 }
@@ -27,6 +28,11 @@ impl<G: Group> ContextHash<G> for VectorContextHash {
     fn add_point(&mut self, p: &G::Point) {
         let mut bytes = vec![0u8; G::Point::BUFFER_SIZE];
         p.to_bytes(&mut bytes[..]);
+        self.context.extend_from_slice(&bytes[..]);
+    }
+    fn add_scalar(&mut self, s: &G::Scalar) {
+        let mut bytes = vec![0u8; G::Scalar::BUFFER_SIZE];
+        s.to_bytes(&mut bytes[..]);
         self.context.extend_from_slice(&bytes[..]);
     }
 
---
+++ core/src/primitives/zkp2/or.rs
@@ -0,0 +1,187 @@
+use rand_core::{CryptoRng, RngCore};
+use crate::foundation::group::Group;
+use crate::foundation::hash::{ContextHash, VectorContextHash};
+use crate::primitives::zkp2::reenc::{ReEncZKP, ReEncZKPChallenge, ReEncZKPCommit, ReEncZKPContext, ReEncZKPProof, ReEncZKPPublicData, ReEncZKPResponse, ReEncZKPWitness};
+use crate::primitives::zkp2::{ZKP, SigmaZKP, SimulableZKP};
+
+// The two ZKPs that we want to combine with an OR
+// The goal is to write everything in terms of these two aliases,
+// so that we can have a macro at some point.
+type ZKP1<G> = ReEncZKP<G>;
+type ZKP2<G> = ReEncZKP<G>;
+
+
+// No more refs to RencZKP below this line ?
+pub struct OrTwoReEncZKP<G> where G: Group {
+    zkp1: ZKP1<G>,
+    zkp2: ZKP2<G>,
+    context: Vec<u8>,
+}
+// Only one of the two witnesses is needed, the other one is None
+pub struct OrTwoReEncZKPWitness<G> where G: Group {
+    zkp1_witness: Option<<ZKP1<G> as ZKP<G>>::Witness>,
+    zkp2_witness: Option<<ZKP2<G> as ZKP<G>>::Witness>,
+}
+pub type OrTwoReEncZKPContext = Vec<u8>;
+pub struct OrTwoReEncZKPCommit<G> where G: Group {
+    zkp1_commit: <ZKP1<G> as SigmaZKP<G>>::Commit,
+    zkp2_commit: <ZKP2<G> as SigmaZKP<G>>::Commit,
+}
+pub struct OrTwoReEncZKPResponse<G> where G: Group {
+    zkp1_response: <ZKP1<G> as SigmaZKP<G>>::Response,
+    zkp2_response: <ZKP2<G> as SigmaZKP<G>>::Response,
+}
+pub struct OrTwoReEncZKPChallenge<G> where G: Group{
+    chal1: <G as Group>::Scalar,
+    chal2: <G as Group>::Scalar,
+}
+// The real challenge is the sum of the two internal challenges
+pub type OrTwoReEncZKPSumChallenges<G> = <G as Group>::Scalar;
+pub struct OrTwoReEncZKPState<G> where G: Group {
+    side: u8, // remember which one is the one that is simulated
+    // when witness is known for zkp1:
+    zkp1_state: Option<<ZKP1<G> as SigmaZKP<G>>::State>,
+    zkp2_simulated: Option<<ZKP2<G> as ZKP<G>>::Proof>,
+    // when witness is known for zkp2:
+    zkp2_state: Option<<ZKP2<G> as SigmaZKP<G>>::State>,
+    zkp1_simulated: Option<<ZKP1<G> as ZKP<G>>::Proof>,
+}
+pub struct OrTwoReEncZKPPublicData<G> where G: Group {
+    zkp1_pubdata: <ZKP1<G> as ZKP<G>>::PublicData,
+    zkp2_pubdata: <ZKP2<G> as ZKP<G>>::PublicData,
+}
+pub struct OrTwoReEncZKPProof<G: Group> {
+    commit: OrTwoReEncZKPCommit<G>,
+    challenge: OrTwoReEncZKPChallenge<G>,
+    response: OrTwoReEncZKPResponse<G>
+}
+
+
+impl<G> OrTwoReEncZKP<G> where G: Group
+{
+    pub fn new(zkp1: ZKP1<G>, zkp2: ZKP2<G>, context: Vec<u8>) -> Self {
+        Self { zkp1, zkp2, context }
+    }
+
+    fn commit<R: RngCore + CryptoRng>(&self, witness: &OrTwoReEncZKPWitness<G>, rng: &mut R)
+                                      -> (OrTwoReEncZKPCommit<G>, OrTwoReEncZKPState<G>) {
+        assert!(witness.zkp1_witness != None || witness.zkp2_witness != None);
+        if witness.zkp1_witness.is_some() {
+            let pf2 = self.zkp2.simulate(rng);
+            let (com1, st1) = self.zkp1.commit(&witness.zkp1_witness.unwrap(), rng);
+            let com = OrTwoReEncZKPCommit {
+                zkp1_commit: com1,
+                zkp2_commit: pf2.commit
+            };
+            let state = OrTwoReEncZKPState {
+                side: 1,
+                zkp1_state: Some(st1),
+                zkp2_simulated: Some(pf2),
+                zkp2_state: None,
+                zkp1_simulated: None,
+            };
+            (com, state)
+        } else {
+            let pf1 = self.zkp1.simulate(rng);
+            let (com2, st2) = self.zkp2.commit(&witness.zkp2_witness.unwrap(), rng);
+            let com = OrTwoReEncZKPCommit {
+                zkp1_commit: pf1.commit,
+                zkp2_commit: com2,
+            };
+            let state = OrTwoReEncZKPState {
+                side: 2,
+                zkp1_state: None,
+                zkp2_simulated: None,
+                zkp2_state: Some(st2),
+                zkp1_simulated: Some(pf1),
+            };
+            (com, state)
+        }
+    }
+
+    fn respond(&self, state: OrTwoReEncZKPState<G>, challenge: OrTwoReEncZKPSumChallenges<G>)
+            -> (OrTwoReEncZKPResponse<G>, OrTwoReEncZKPChallenge<G>) {
+        assert!(state.side == 1 || state.side == 2);
+        if state.side == 1 {
+            assert!(state.zkp1_state.is_some());
+            assert!(state.zkp2_simulated.is_some());
+            let st2 = state.zkp2_simulated.unwrap();
+            let chal1 = st2.challenge - &challenge;
+            let resp1 = self.zkp1.respond(&state.zkp1_state.unwrap(), &chal1);
+            let chal = OrTwoReEncZKPChallenge {
+                chal1: chal1,
+                chal2: st2.challenge,
+            };
+            let resp = OrTwoReEncZKPResponse {
+                zkp1_response: resp1,
+                zkp2_response: st2.response,
+            };
+            (resp, chal)
+        } else {
+            assert!(state.zkp2_state.is_some());
+            assert!(state.zkp1_simulated.is_some());
+            let st1 = state.zkp1_simulated.unwrap();
+            let chal2 = st1.challenge - &challenge;
+            let resp2 = self.zkp2.respond(&state.zkp2_state.unwrap(), &chal2);
+            let chal = OrTwoReEncZKPChallenge {
+                chal1: st1.challenge,
+                chal2: chal2,
+            };
+            let resp = OrTwoReEncZKPResponse {
+                zkp1_response: st1.response,
+                zkp2_response: resp2,
+            };
+            (resp, chal)
+        }
+    }
+
+    fn get_challenge(&self, commit: &OrTwoReEncZKPCommit<G>)
+                     -> OrTwoReEncZKPSumChallenges<G> {
+        let mut buf = VectorContextHash::new(self.context.clone());
+        let chal1 = self.zkp1.get_challenge(&commit.zkp1_commit);
+        let chal2 = self.zkp2.get_challenge(&commit.zkp2_commit);
+        <VectorContextHash as ContextHash<G>>::add_scalar(&mut buf, &chal1);
+        <VectorContextHash as ContextHash<G>>::add_scalar(&mut buf, &chal2);
+        <VectorContextHash as ContextHash<G>>::hash_to_scalar(&mut buf)
+    }
+    fn verify(&self, commit: &OrTwoReEncZKPCommit<G>, sum_challenges: &OrTwoReEncZKPSumChallenges<G>, challenge: &OrTwoReEncZKPChallenge<G>, response: &OrTwoReEncZKPResponse<G>)
+              -> bool {
+        let pf1 = ReEncZKPProof {
+            commit: commit.zkp1_commit,
+            challenge: challenge.chal1,
+            response: response.zkp1_response,
+        };
+        let pf2 = ReEncZKPProof {
+            commit: commit.zkp2_commit,
+            challenge: challenge.chal2,
+            response: response.zkp2_response,
+        };
+        let chal = pf1.challenge + &pf2.challenge;
+        chal == *sum_challenges && self.zkp1.verify(&pf1) && self.zkp2.verify(&pf2)
+    }
+}
+
+impl<G> ZKP<G> for OrTwoReEncZKP<G> where G: Group{
+    type PublicData = OrTwoReEncZKPPublicData<G>;
+    type Witness = OrTwoReEncZKPWitness<G>;
+    type Context = OrTwoReEncZKPContext;
+    type Proof = OrTwoReEncZKPProof<G>;
+    fn prove<R: RngCore + CryptoRng>(&self, witness: &Self::Witness, rng: &mut R)
+                                     -> (Self::Proof) {
+        let (commit, state) = Self::commit(&self, witness, rng);
+        let sum_chal = Self::get_challenge(&self, &commit);
+        let (response, chal) = Self::respond(&self, state, sum_chal);
+        (
+            OrTwoReEncZKPProof {
+                commit: commit,
+                challenge: chal,
+                response: response,
+            }
+        )
+    }
+
+    fn verify(&self, proof: &Self::Proof) -> bool {
+        let sum_chal = Self::get_challenge(&self, &proof.commit);
+        Self::verify(&self, &proof.commit, &sum_chal, &proof.challenge, &proof.response)
+    }
+}
