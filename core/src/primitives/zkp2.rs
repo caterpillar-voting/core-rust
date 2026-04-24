b7a8536d74ca1f570bf3702735799942728b511f
Pierrick Gaudry <pierrick.gaudry@loria.fr>
2026-04-24 16:36:17 +0200

    wip: still not recursive, but making progress

--- core/src/primitives/zkp2.rs
+++ core/src/primitives/zkp2.rs
@@ -17,7 +17,7 @@ pub trait SigmaZKP<G: Group> : ZKP<G> {
     type Commit;    // The data sent by Prover in first step
     type Challenge; // The data sent by Verifier in second step
     type Response;  // The data sent by Prover in third step
-    type State;     // The data kept by Prover between steps
+    type State : Clone + Copy;     // The data kept by Prover between steps
     fn commit<R: RngCore + CryptoRng>(&self, witness: &Self::Witness, rng: &mut R) -> (Self::Commit, Self::State);
     fn get_challenge(&self, commit: &Self::Commit) -> (Self::Challenge);
     fn respond(&self, state: &Self::State, challenge: &Self::Challenge) -> (Self::Response);
