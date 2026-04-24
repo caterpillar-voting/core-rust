ab2f58f344d34c1009ed97a84bbfb5018d22c77b
Pierrick Gaudry <pierrick.gaudry@loria.fr>
2026-04-24 13:47:28 +0200

    Trying or-proof; doesn't work yet: doesn't recurse.

--- core/src/primitives/zkp2.rs
+++ core/src/primitives/zkp2.rs
@@ -1,4 +1,5 @@
 pub mod reenc;
+pub mod or;
 
 use rand_core::{CryptoRng, RngCore};
 use crate::foundation::group::Group;
@@ -8,8 +9,8 @@ pub trait ZKP<G: Group> {
     type Witness;
     type Context;
     type Proof;
-    fn prove<R: RngCore + CryptoRng>(&self, public_data: &Self::PublicData, witness: &Self::Witness, context: &Self::Context, rng: &mut R) -> (Self::Proof);
-    fn verify(&self, public_data: &Self::PublicData, context: &Self::Context, proof: &Self::Proof) -> bool;
+    fn prove<R: RngCore + CryptoRng>(&self, witness: &Self::Witness, rng: &mut R) -> (Self::Proof);
+    fn verify(&self, proof: &Self::Proof) -> bool;
 }
 
 pub trait SigmaZKP<G: Group> : ZKP<G> {
@@ -17,11 +18,11 @@ pub trait SigmaZKP<G: Group> : ZKP<G> {
     type Challenge; // The data sent by Verifier in second step
     type Response;  // The data sent by Prover in third step
     type State;     // The data kept by Prover between steps
-    fn commit<R: RngCore + CryptoRng>(&self, public_data: &Self::PublicData, witness: &Self::Witness, context: &Self::Context) -> (Self::Commit, Self::State);
-    fn get_challenge(&self, public_data: &Self::PublicData, context: &Self::Context, commit: &Self::Commit) -> (Self::Challenge);
-    fn respond(&self, public_data: &Self::PublicData, state: &Self::State, context: &Self::Context, challenge: &Self::Challenge) -> (Self::Response);
+    fn commit<R: RngCore + CryptoRng>(&self, witness: &Self::Witness, rng: &mut R) -> (Self::Commit, Self::State);
+    fn get_challenge(&self, commit: &Self::Commit) -> (Self::Challenge);
+    fn respond(&self, state: &Self::State, challenge: &Self::Challenge) -> (Self::Response);
 }
 
 pub trait SimulableZKP<G: Group> : SigmaZKP<G> {
-    fn simulate<R: RngCore + CryptoRng>(&self, public_data: &Self::PublicData, context: &Self::Context) -> (Self::Proof);
+    fn simulate<R: RngCore + CryptoRng>(&self, rng: &mut R) -> (Self::Proof);
 }
--- core/src/primitives/zkp2/reenc.rs
+++ core/src/primitives/zkp2/reenc.rs
@@ -4,8 +4,8 @@ use crate::foundation::hash::{ContextHash, VectorContextHash};
 use crate::primitives::zkp2::{ZKP, SigmaZKP, SimulableZKP};
 
 pub struct ReEncZKP<G> where G: Group {
-    public_data: ReEncZKPPublicData<G>,
-    context: Vec<u8>,
+    pub public_data: ReEncZKPPublicData<G>,
+    pub context: Vec<u8>,
 }
 
 pub type ReEncZKPWitness<G> = <G as Group>::Scalar;
@@ -16,9 +16,9 @@ pub type ReEncZKPChallenge<G> = <G as Group>::Scalar;
 pub type ReEncZKPState<G> = (<G as Group>::Scalar, <G as Group>::Scalar);
 
 pub struct ReEncZKPProof<G: Group> {
-    commit: ReEncZKPCommit<G>,
-    challenge: ReEncZKPChallenge<G>,
-    response: ReEncZKPResponse<G>
+    pub commit: ReEncZKPCommit<G>,
+    pub challenge: ReEncZKPChallenge<G>,
+    pub response: ReEncZKPResponse<G>
 }
 
 pub struct ReEncZKPPublicData<G> where G: Group {
@@ -55,29 +55,47 @@ impl<G> ReEncZKP<G> where G: Group {
         state.0 + &(state.1 * challenge)
     }
 
-    fn get_challenge(&self, public_data: &ReEncZKPPublicData<G>, context: &ReEncZKPContext, commit: &ReEncZKPCommit<G>)
+    fn get_challenge(&self, commit: &ReEncZKPCommit<G>)
             -> ReEncZKPChallenge<G> {
-        let mut buf = VectorContextHash::new(context.clone());
-        <VectorContextHash as ContextHash<G>>::add_point(&mut buf, &public_data.public_key);
-        <VectorContextHash as ContextHash<G>>::add_point(&mut buf, &public_data.ciphertext.0);
-        <VectorContextHash as ContextHash<G>>::add_point(&mut buf, &public_data.ciphertext.1);
-        <VectorContextHash as ContextHash<G>>::add_point(&mut buf, &public_data.ciphertext_rnd.0);
-        <VectorContextHash as ContextHash<G>>::add_point(&mut buf, &public_data.ciphertext_rnd.1);
+        let mut buf = VectorContextHash::new(self.context.clone());
+        <VectorContextHash as ContextHash<G>>::add_point(&mut buf, &self.public_data.public_key);
+        <VectorContextHash as ContextHash<G>>::add_point(&mut buf, &self.public_data.ciphertext.0);
+        <VectorContextHash as ContextHash<G>>::add_point(&mut buf, &self.public_data.ciphertext.1);
+        <VectorContextHash as ContextHash<G>>::add_point(&mut buf, &self.public_data.ciphertext_rnd.0);
+        <VectorContextHash as ContextHash<G>>::add_point(&mut buf, &self.public_data.ciphertext_rnd.1);
         <VectorContextHash as ContextHash<G>>::add_point(&mut buf, &commit.0);
         <VectorContextHash as ContextHash<G>>::add_point(&mut buf, &commit.1);
         <VectorContextHash as ContextHash<G>>::hash_to_scalar(&mut buf)
     }
 
-    fn verify(&self, public_data: &ReEncZKPPublicData<G>, context: &ReEncZKPContext, commit: &ReEncZKPCommit<G>, challenge: &ReEncZKPChallenge<G>, response: &ReEncZKPResponse<G>)
+    fn verify(&self, commit: &ReEncZKPCommit<G>, challenge: &ReEncZKPChallenge<G>, response: &ReEncZKPResponse<G>)
             -> bool {
         let phir = self.phi(&response);
-        let z0 = public_data.ciphertext_rnd.0 - &public_data.ciphertext.0;
-        let z1 = public_data.ciphertext_rnd.1 - &public_data.ciphertext.1;
+        let z0 = self.public_data.ciphertext_rnd.0 - &self.public_data.ciphertext.0;
+        let z1 = self.public_data.ciphertext_rnd.1 - &self.public_data.ciphertext.1;
         let cz0 = z0 * &challenge;
         let cz1 = z1 * &challenge;
         let p0 = cz0 + &commit.0;
         let p1 = cz1 + &commit.1;
         phir.0 == p0 && phir.1 == p1
+    }
+
+    fn simulate<R: RngCore + CryptoRng>(&self, rng: &mut R) -> (ReEncZKPProof<G>) {
+        let challenge = G::scalar_random(rng);
+        let response = G::scalar_random(rng);
+        let z0 = self.public_data.ciphertext_rnd.0 - &self.public_data.ciphertext.0;
+        let z1 = self.public_data.ciphertext_rnd.1 - &self.public_data.ciphertext.1;
+        let phir = self.phi(&response);
+        let com0 = phir.0 - &(z0 * &challenge);
+        let com1 = phir.1 - &(z1 * &challenge);
+        let commit = (com0, com1);
+        (
+            ReEncZKPProof {
+                commit,
+                challenge,
+                response,
+            }
+        )
     }
 }
 
@@ -86,24 +104,45 @@ impl<G> ZKP<G> for ReEncZKP<G> where G: Group{
     type Witness = ReEncZKPWitness<G>;
     type Context = ReEncZKPContext;
     type Proof = ReEncZKPProof<G>;
-    fn prove<R: RngCore + CryptoRng>(&self, public_data: &Self::PublicData, witness: &Self::Witness, context: &Self::Context, rng: &mut R)
+    fn prove<R: RngCore + CryptoRng>(&self, witness: &Self::Witness, rng: &mut R)
             -> (Self::Proof) {
         let (commit, state) = Self::commit(&self, witness, rng);
-        let chal = Self::get_challenge(&self, public_data, context, &commit);
+        let chal = Self::get_challenge(&self, &commit);
         let response = Self::respond(&self, &state, &chal);
         (
             ReEncZKPProof {
-                commit,
+                commit: commit,
                 challenge: chal,
-                response,
+                response: response,
             }
         )
     }
 
-    fn verify(&self, public_data: &Self::PublicData, context: &Self::Context, proof: &Self::Proof) -> bool {
-        let chal = Self::get_challenge(&self, public_data, context, &proof.commit);
+    fn verify(&self, proof: &Self::Proof) -> bool {
+        let chal = Self::get_challenge(&self, &proof.commit);
         proof.challenge == chal &&
-        Self::verify(&self, public_data, context, &proof.commit, &proof.challenge, &proof.response)
+        Self::verify(&self, &proof.commit, &proof.challenge, &proof.response)
+    }
+}
 
+impl<G> SigmaZKP<G> for ReEncZKP<G> where G: Group {
+    type Commit = ReEncZKPCommit<G>;
+    type Challenge = ReEncZKPChallenge<G>;
+    type Response = ReEncZKPResponse<G>;
+    type State = ReEncZKPState<G>;
+    fn commit<R: RngCore + CryptoRng>(&self, witness: &Self::Witness, rng: &mut R) -> (Self::Commit, Self::State) {
+        Self::commit(&self, witness, rng)
+    }
+    fn get_challenge(&self, commit: &Self::Commit) -> (Self::Challenge) {
+        Self::get_challenge(&self, commit)
+    }
+    fn respond(&self, state: &Self::State, challenge: &Self::Challenge) -> (Self::Response) {
+        Self::respond(&self, state, challenge)
+    }
+}
+
+impl<G> SimulableZKP<G> for ReEncZKP<G> where G: Group {
+    fn simulate<R: RngCore + CryptoRng>(&self, rng: &mut R) -> (Self::Proof) {
+        Self::simulate(&self, rng)
     }
 }
