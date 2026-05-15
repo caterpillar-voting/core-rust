af5440c24be03e38fc2ca893d7a9f181b6b098ea
Florian Moser <git@famoser.ch>
2026-05-15 09:05:13 +0200

    feat: Integrate htdh2 into second approach

--- core/src/primitives/zkp/proof_builder/el_gamal.rs
+++ core/src/primitives/zkp/proof_builder/el_gamal.rs
@@ -2,6 +2,7 @@ use crate::foundation::group::Group;
 use crate::primitives::zkp::proof::{Claim, Knowledge};
 use crate::primitives::zkp::proof_builder::ProofBuilder;
 use crate::primitives::zkp::statement::Statement;
+use crate::primitives::zkp::{NIZKProof, ZKProof};
 use crate::utils::tree::BooleanTree::Leaf;
 
 pub struct ReEncProofBuilder<G: Group> {
@@ -51,5 +52,64 @@ impl<G: Group> ProofBuilder<G> for EncProofBuilder<G> {
         let knowledge = Leaf(self.randomness);
 
         (claim, knowledge)
+    }
+}
+
+
+pub struct HTDH2ProofBuilder<G: Group> {
+    g0: G::Point,
+    uv: (G::Point, G::Point),
+    g0r: G::Point,
+    randomness: Option<G::Scalar>,
+}
+
+impl<G: Group> HTDH2ProofBuilder<G> {
+    pub fn new(g0: G::Point, uv: (G::Point, G::Point), g0r: G::Point, randomness: Option<G::Scalar>) -> Self {
+        Self { g0, uv, g0r, randomness }
+    }
+}
+
+impl<G: Group> ProofBuilder<G> for HTDH2ProofBuilder<G> {
+    fn build(&self) -> (Claim<G>, Knowledge<G>) {
+        let w0 = Statement::<G>::new(G::basepoint(), self.uv.0);
+        let w1 = Statement::<G>::new(self.g0, self.g0r);
+
+        let claim: Claim<G> = Leaf(Box::new([w0, w1]));
+        let knowledge = Leaf(self.randomness);
+
+        (claim, knowledge)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::foundation::group::Group;
+    use crate::foundation::group::ristretto::RistrettoGroup;
+    use crate::foundation::hash::VectorContextHash;
+    use crate::primitives::zkp::_test_utils::create_elgamal_enc0_and_enc1;
+    use crate::primitives::zkp::proof_builder::TreeProofBuilder;
+    use crate::primitives::zkp::proof_builder::el_gamal::{EncProofBuilder, ReEncProofBuilder};
+    use crate::utils::tree::BooleanTree::{Leaf, Or};
+    use rand::thread_rng;
+
+    type Curve = RistrettoGroup;
+
+    #[test]
+    fn htdh2_proof() {
+        let mut rng = thread_rng();
+
+        let (pk, (uv, r), _) = create_elgamal_enc0_and_enc1(&mut rng);
+
+        let g_0 = Curve::independent_generators::<1>(b"HTDH2ZKP")[0];
+        let g_0_r = g_0 * r;
+        let htdh2 = HTDH2ProofBuilder::<Curve>::new(g_0, uv, g_0_r, Some(r));
+
+        let (zk_proof, knowledge) = ZKProof::from_builder(&htdh2);
+
+        let nizkp = NIZKProof::new(zk_proof, VectorContextHash::default());
+        let proof = nizkp.prove(&mut rng, &knowledge);
+
+        assert!(nizkp.verify(&proof))
     }
 }
